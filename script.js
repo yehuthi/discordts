9b257cc94fb5361f5ed1fe9545d3d7adaf3766bb
Yehuthi <93691725+yehuthi@users.noreply.github.com>
2022-03-19 04:13:27 +0200

    Improve analytics

--- script.js
+++ script.js
@@ -14,36 +14,36 @@ function updateView() {
 	const date = new Date(dateInput.value);
 	const epoch = (date / 1000) | 0;
 
-	const epochCopyFlag = flag => () => {
+	const epochCopyFlag = (flag, type) => () => {
 		navigator.clipboard.writeText(`<t:${epoch}:${flag}>`);
-		gtag("event", "copy_output", { date, flag });
+		gtag("event", "copy_output", { date, type });
 	};
 
 	tst.textContent = date.toLocaleTimeString(previewLocale, {
 		timeStyle: "short",
 		hour12: false,
 	});
-	tst.onclick = epochCopyFlag("t");
+	tst.onclick = epochCopyFlag("t", "Time (Short)");
 
 	tsT.textContent = `:${date.getSeconds().toString().padStart(2, "0")}`;
-	tsT.onclick = epochCopyFlag("T");
+	tsT.onclick = epochCopyFlag("T", "Time (Long)");
 
 	tsd.textContent = date.toLocaleDateString(previewLocale, {
 		day: "numeric",
 		month: "numeric",
 		year: "numeric",
 	});
-	tsd.onclick = epochCopyFlag("d");
+	tsd.onclick = epochCopyFlag("d", "Date (Short)");
 
 	tsD.textContent = date.toLocaleDateString(previewLocale, {
 		dateStyle: "long",
 	});
-	tsD.onclick = epochCopyFlag("D");
+	tsD.onclick = epochCopyFlag("D", "Date (Long)");
 
 	tsF.textContent = date.toLocaleString(previewLocale, {
 		weekday: "long",
 	});
-	tsF.onclick = epochCopyFlag("F");
+	tsF.onclick = epochCopyFlag("F", "Datetime (Long)");
 
 	tsf.textContent = date.toLocaleString(previewLocale, {
 		month: "long",
@@ -54,13 +54,13 @@ function updateView() {
 	});
 	tsf.onclick = () => {
 		navigator.clipboard.writeText(`<t:${epoch}>`);
-		gtag("event", "copy_output", { date, flag: "f" });
+		gtag("event", "copy_output", { date, type: "Datetime (Short)" });
 	};
 
 	timeago.cancel(tsR);
 	tsR.setAttribute("datetime", date.toISOString());
 	timeago.render(tsR);
-	tsR.onclick = epochCopyFlag("R");
+	tsR.onclick = epochCopyFlag("R", "Relative");
 }
 
 function init() {
